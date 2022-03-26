9b640ec98813274222b6fde6f459aca624d12838
Maydara <cherif.mohamed86@gmail.com>
2022-03-26 01:34:38 +0100

    Install Tailwind CSS and add the theme colors

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#6E1F46',
+        secondary: '#EE17BA',
+        neutral: '#888888',
+      },
+    },
+  },
+  plugins: [],
+}
